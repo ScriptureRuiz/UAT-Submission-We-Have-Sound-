9c065770a04ddba6462ee50b0bdc36b8c43fac36
Martin Ruiz <105525646+ScriptureRuiz@users.noreply.github.com>
2022-06-05 13:05:39 -0500

    Add files via upload

---
+++ weHaveSound.js
@@ -0,0 +1,15 @@
+/*Martin Ruiz
+    6/3/2022
+A: We Have Sound*/
+
+//this function is plays audio once the play button is pressed.
+function playMusic(){
+let audio=new Audio("SummerBreeze Guitar HipHop Instrumental.mp4")
+audio.play();
+}
+
+
+
+
+
+
